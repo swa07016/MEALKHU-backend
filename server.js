4df1e52227725ea39e23f92cf02f012f8261c65e
isakchoe <50509176+isakchoe@users.noreply.github.com>
2020-08-20 21:09:50 +0900

    몽고 db로 read 기능 성공!

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs");
 const cors = require('cors');
+const mongoose = require('mongoose');
+const { strict } = require("assert");
 
 
 const app = express();
@@ -14,6 +16,29 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+// 디비 연결을 합시다.
+
+mongoose.connect('mongodb://localhost:27017/seoul', {useNewUrlParser:true})
+
+const diningSchema ={
+  title : String,
+  content: String
+};
+
+const Dining = mongoose.model('Dining', diningSchema)
+
+const gdiningSchema = {
+  title : String,
+  content : String
+};
+
+const Gdining  = mongoose.model('Gdining', gdiningSchema)
+
+
+
+
+
+
 // 홈 화면 (임시)
 app.get('/', function(req,res){
   res.send('hello')
@@ -21,41 +46,31 @@ app.get('/', function(req,res){
 
 // 서울, 국제 api 
 app.get("/api/global", (req, res) => {
-  fs.readFile('db_global.json', 'utf8', (err, data) => {
-    res.send(data);
+  Gdining.find(function(err, foundGdinings){
+    if(!err){
+      res.send(foundGdinings)
+    }
+    else {
+      res.send(err)
+    }
   });
+
+
 });
 
 app.get("/api/seoul", (req, res) => {
-  fs.readFile('db_seoul.json', 'utf8', (err, data) => {
-    res.send(data);
+  Dining.find(function(err, foundDinings){
+    if(!err){
+      res.send(foundDinings)
+    }
+    else{
+      res.send(err);
+    }
   });
-});
+})
 
 
-// 디비 연결을 합시다.
 
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-
-// Connection URL
-const url = 'mongodb://localhost:27017';
-
-// Database Name
-const dbName = 'myproject';
-
-// Create a new MongoClient
-const client = new MongoClient(url);
-
-// Use connect method to connect to the Server
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-  client.close();
-});
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
