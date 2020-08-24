d53d81ea6d418036877d89483fb0fdb7b71f2e79
isakchoe <50509176+isakchoe@users.noreply.github.com>
2020-08-24 10:59:34 +0900

    오타수정

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs");
-// const cors = require('cors');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const { strict } = require("assert");
 
