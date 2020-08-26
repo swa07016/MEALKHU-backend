6f59883e98268c33f31ed70bf72bf85725cda635
isakchoe <50509176+isakchoe@users.noreply.github.com>
2020-08-26 11:47:48 +0900

    success

--- server.js
+++ server.js
@@ -23,40 +23,12 @@ app.use(cors())
 
 
 
-
+//cors 정책 
 let corsOptions = {
   origin: 'https://mealkhu-test.herokuapp.com', // 허용되는 Origin
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
  
-
-app.get('/api/global', cors(corsOptions), function (req, res, next) {
-  G_dining.find(function(err, foundG_dinings){
-    if(!err){
-      res.send(foundG_dinings)
-    }
-    else {
-      res.send(err)
-    } 
-})
-});
-
-
-
-app.get('/api/seoul', cors(corsOptions), function (req, res, next) {
-  S_dining.find(function(err, foundS_dinings){
-    if(!err){
-      res.send(foundS_dinings)
-    }
-    else {
-      res.send(err)
-    } 
-})
-});
-
-
-
-
 
 
 // 디비 연결을 합시다.
@@ -79,38 +51,37 @@ const G_dining  = mongoose.model('G_dining', g_diningSchema)
 
 
 
-
-
-
 // 홈 화면 (임시)
 app.get('/', function(req,res){
   res.send('hello')
 })
 
-// 국제 api 
-// app.get("/api/global",  (req, res) => {
-//   G_dining.find(function(err, foundG_dinings){
-//     if(!err){
-//       res.send(foundG_dinings)
-//     }
-//     else {
-//       res.send(err)
-//     }
-//   });
+ 
+//국제 api
+app.get('/api/global', cors(corsOptions), function (req, res, next) {
+  G_dining.find(function(err, foundG_dinings){
+    if(!err){
+      res.send(foundG_dinings)
+    }
+    else {
+      res.send(err)
+    } 
+})
+});
 
 
-// });
-// //서울 api
-// app.get("/api/seoul", (req, res) => {
-//   S_dining.find(function(err, foundS_dinings){
-//     if(!err){
-//       res.send(foundS_dinings)
-//     }
-//     else{
-//       res.send(err);
-//     }
-//   });
-// })
+//서울 api
+app.get('/api/seoul', cors(corsOptions), function (req, res, next) {
+  S_dining.find(function(err, foundS_dinings){
+    if(!err){
+      res.send(foundS_dinings)
+    }
+    else {
+      res.send(err)
+    } 
+})
+});
+
 
 
 
